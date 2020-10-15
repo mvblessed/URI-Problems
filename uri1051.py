2a1034c866ea393dbd6e5c74effb004fb89ce7b6
Felipe Seda <63421531+fseda@users.noreply.github.com>
2020-10-15 20:12:46 -0300

    Update uri1051.py

--- uri1051.py
+++ uri1051.py
@@ -3,10 +3,10 @@ if 0 <= salario <= 2000:
     print('Isento')
 elif 2000 < salario <= 3000:
     imposto = ((salario-2000)*8)/100
-    print('R$ %.2f' % imposto)
+    print(f'R$ {imposto:.2f}')
 elif 3000 < salario <= 4500:
-    imposto = ((1000*8)/100)+(((salario-3000)*18)/100)
-    print('R$ %.2f' % imposto)
+    imposto = ((1000 * 8) / 100)+(((salario - 3000) * 18) / 100)
+    print(f'R$ {imposto:.2f}')
 else:
-    imposto = ((1000*8)/100)+((1500*18)/100)+(((salario-4500)*28)/100)
-    print('R$ %.2f' % imposto)
+    imposto = (( 1000 * 8) / 100) + ((1500 * 18) / 100)+(((salario - 4500) * 28) / 100)
+    print(f'R$ {imposto:.2f}')

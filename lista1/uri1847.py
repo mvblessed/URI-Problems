05d6548c83356bc93b5cc374347a601e1adc4c5c
Felipe Seda <63421531+fseda@users.noreply.github.com>
2020-10-15 20:19:06 -0300

    Update uri1847.py

--- lista1/uri1847.py
+++ lista1/uri1847.py
@@ -1,15 +1,15 @@
 a, b, c = list(map(int, input().split()))
 if a > b and (b < c or b == c):
   print(':)')
-elif a < b and (b > c or b ==c):
+elif a < b and (b > c or b == c):
   print(':(')
 elif a < b < c and (b-a > c-b):
   print(':(')
-elif a < b < c and ((b-a == c-b) or (b-a < c-b)):
+elif a < b < c and ((b - a == c - b) or (b - a < c - b)):
   print(':)')
-elif a > b > c and (a-b > b-c):
+elif a > b > c and (a - b > b - c):
   print(':)')
-elif a > b > c and ((a-b == b-c) or (a-b < b-c)):
+elif a > b > c and ((a - b == b - c) or (a - b < b - c)):
   print(':(')
 elif a == b and b > c:
   print(':(')

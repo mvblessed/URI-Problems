7ae847e1128a48ed575129fc7a3d23233272a454
Felipe Seda <fsra1610@gmail.com>
2020-10-20 13:54:40 -0300

    upload problem 1018

--- lista2/uri1018.py
+++ lista2/uri1018.py
@@ -1,9 +1,18 @@
-notas = int(input())
-valores = [100, 50, 20, 10, 5, 2, 1]
-print(notas)
-for i in range(7):
-    nota, notas = divmod(notas, valores[i])
-    print('%d nota(s) de R$ %d,00' %(nota, valores[i]))
+N = int(input())
 
+cem  = N // 100
+cinq = (N % 100) // 50
+vint = ((N % 100) % 50) // 20
+dez  = (((N % 100) % 50) % 20) // 10
+cinc = ((((N % 100) % 50) % 20 ) % 10) // 5
+dois = (((((N % 100) % 50) % 20) % 10) % 5) // 2
+um   = ((((((N % 100) % 50) % 20) % 10) % 5) % 2) // 1
 
-for i in range()
+print(N)
+print(f'{cem} nota(s) de R$ 100,00')
+print(f'{cinq} nota(s) de R$ 50,00')
+print(f'{vint} nota(s) de R$ 20,00')
+print(f'{cinc} nota(s) de R$ 10,00')
+print(f'{dois} nota(s) de R$ 5,00')
+print(f'{um} nota(s) de R$ 1,00')
+
